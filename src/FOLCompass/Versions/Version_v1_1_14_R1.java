8e483fe3ade808123c17c914c7f323b2e36d93ff
Kugana <wayne87060271@gmail.com>
2021-04-06 11:30:45 +0800

    Add files via upload

---
+++ src/FOLCompass/Versions/Version_v1_1_14_R1.java
@@ -0,0 +1,33 @@
+package FOLCompass.Versions;
+
+
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import net.minecraft.server.v1_14_R1.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_14_R1.PacketPlayOutChat;
+import net.minecraft.server.v1_14_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_14_R1.PacketPlayOutTitle.EnumTitleAction;
+
+
+
+public class Version_v1_1_14_R1 implements NMS {
+
+	@Override
+	public void sendTitle(Player p, String titleMessage, int fadIn, int onScreen, int fedeOut) {
+		PacketPlayOutTitle title = new PacketPlayOutTitle(EnumTitleAction.TITLE,
+				ChatSerializer.a("{\"text\": \"" + titleMessage + "\"}"), fadIn, onScreen, fedeOut);
+
+		((CraftPlayer) p).getHandle().playerConnection.sendPacket(title);
+
+	}
+
+	@Override
+	public void sendActionbar(Player p, String message) {
+		PacketPlayOutChat packet = new PacketPlayOutChat(ChatSerializer.a("{\"text\": \"" + message + "\"}"));
+		((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+
+	}
+
+
+}
